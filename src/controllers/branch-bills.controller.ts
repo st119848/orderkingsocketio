c5960e0702090f04f8d84cd23b8cb63e274cc7a0
bankkeez <sethitikul@gmail.com>
2019-09-22 23:37:55 +0700

    add transactions for driver and rest

--- src/controllers/branch-bills.controller.ts
+++ src/controllers/branch-bills.controller.ts
@@ -3,8 +3,8 @@ import {
   CountSchema,
   Filter,
   repository,
-  Where,
-} from '@loopback/repository';
+  Where
+} from "@loopback/repository";
 import {
   del,
   get,
@@ -13,97 +13,94 @@ import {
   param,
   patch,
   post,
-  requestBody,
-} from '@loopback/rest';
-import {
-  Branch,
-  Bills,
-} from '../models';
-import {BranchRepository} from '../repositories';
+  requestBody
+} from "@loopback/rest";
+import { Branch, Bills } from "../models";
+import { BranchRepository } from "../repositories";
 
 export class BranchBillsController {
   constructor(
-    @repository(BranchRepository) protected branchRepository: BranchRepository,
-  ) { }
+    @repository(BranchRepository) protected branchRepository: BranchRepository
+  ) {}
 
-  @get('/branches/{id}/bills', {
+  @get("/branches/{id}/bills", {
     responses: {
-      '200': {
-        description: 'Array of Bills\'s belonging to Branch',
+      "200": {
+        description: "Array of Bills's belonging to Branch",
         content: {
-          'application/json': {
-            schema: {type: 'array', items: getModelSchemaRef(Bills)},
-          },
-        },
-      },
-    },
+          "application/json": {
+            schema: { type: "array", items: getModelSchemaRef(Bills) }
+          }
+        }
+      }
+    }
   })
   async find(
-    @param.path.number('id') id: number,
-    @param.query.object('filter') filter?: Filter<Bills>,
+    @param.path.number("id") id: number,
+    @param.query.object("filter") filter?: Filter<Bills>
   ): Promise<Bills[]> {
     return this.branchRepository.bills(id).find(filter);
   }
 
-  @post('/branches/{id}/bills', {
+  @post("/branches/{id}/bills", {
     responses: {
-      '200': {
-        description: 'Branch model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Bills)}},
-      },
-    },
+      "200": {
+        description: "Branch model instance",
+        content: { "application/json": { schema: getModelSchemaRef(Bills) } }
+      }
+    }
   })
   async create(
-    @param.path.number('id') id: typeof Branch.prototype.id,
+    @param.path.number("id") id: typeof Branch.prototype.id,
     @requestBody({
       content: {
-        'application/json': {
+        "application/json": {
           schema: getModelSchemaRef(Bills, {
-            title: 'NewBillsInBranch',
-            exclude: ['id'],
-            optional: ['branchId']
-          }),
-        },
-      },
-    }) bills: Omit<Bills, 'id'>,
+            exclude: ["id"],
+            optional: ["branchId"]
+          })
+        }
+      }
+    })
+    bills: Omit<Bills, "id">
   ): Promise<Bills> {
     return this.branchRepository.bills(id).create(bills);
   }
 
-  @patch('/branches/{id}/bills', {
+  @patch("/branches/{id}/bills", {
     responses: {
-      '200': {
-        description: 'Branch.Bills PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
+      "200": {
+        description: "Branch.Bills PATCH success count",
+        content: { "application/json": { schema: CountSchema } }
+      }
+    }
   })
   async patch(
-    @param.path.number('id') id: number,
+    @param.path.number("id") id: number,
     @requestBody({
       content: {
-        'application/json': {
-          schema: getModelSchemaRef(Bills, {partial: true}),
-        },
-      },
+        "application/json": {
+          schema: getModelSchemaRef(Bills, { partial: true })
+        }
+      }
     })
     bills: Partial<Bills>,
-    @param.query.object('where', getWhereSchemaFor(Bills)) where?: Where<Bills>,
+    @param.query.object("where", getWhereSchemaFor(Bills)) where?: Where<Bills>
   ): Promise<Count> {
     return this.branchRepository.bills(id).patch(bills, where);
   }
 
-  @del('/branches/{id}/bills', {
+  @del("/branches/{id}/bills", {
     responses: {
-      '200': {
-        description: 'Branch.Bills DELETE success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
+      "200": {
+        description: "Branch.Bills DELETE success count",
+        content: { "application/json": { schema: CountSchema } }
+      }
+    }
   })
   async delete(
-    @param.path.number('id') id: number,
-    @param.query.object('where', getWhereSchemaFor(Bills)) where?: Where<Bills>,
+    @param.path.number("id") id: number,
+    @param.query.object("where", getWhereSchemaFor(Bills)) where?: Where<Bills>
   ): Promise<Count> {
     return this.branchRepository.bills(id).delete(where);
   }
--- src/controllers/category-product.controller.ts
+++ src/controllers/category-product.controller.ts
@@ -3,8 +3,8 @@ import {
   CountSchema,
   Filter,
   repository,
-  Where,
-} from '@loopback/repository';
+  Where
+} from "@loopback/repository";
 import {
   del,
   get,
@@ -13,97 +13,97 @@ import {
   param,
   patch,
   post,
-  requestBody,
-} from '@loopback/rest';
-import {
-  Category,
-  Product,
-} from '../models';
-import {CategoryRepository} from '../repositories';
+  requestBody
+} from "@loopback/rest";
+import { Category, Product } from "../models";
+import { CategoryRepository } from "../repositories";
 
 export class CategoryProductController {
   constructor(
-    @repository(CategoryRepository) protected categoryRepository: CategoryRepository,
-  ) { }
+    @repository(CategoryRepository)
+    protected categoryRepository: CategoryRepository
+  ) {}
 
-  @get('/categories/{id}/products', {
+  @get("/categories/{id}/products", {
     responses: {
-      '200': {
-        description: 'Array of Product\'s belonging to Category',
+      "200": {
+        description: "Array of Product's belonging to Category",
         content: {
-          'application/json': {
-            schema: {type: 'array', items: getModelSchemaRef(Product)},
-          },
-        },
-      },
-    },
+          "application/json": {
+            schema: { type: "array", items: getModelSchemaRef(Product) }
+          }
+        }
+      }
+    }
   })
   async find(
-    @param.path.number('id') id: number,
-    @param.query.object('filter') filter?: Filter<Product>,
+    @param.path.number("id") id: number,
+    @param.query.object("filter") filter?: Filter<Product>
   ): Promise<Product[]> {
     return this.categoryRepository.products(id).find(filter);
   }
 
-  @post('/categories/{id}/products', {
+  @post("/categories/{id}/products", {
     responses: {
-      '200': {
-        description: 'Category model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Product)}},
-      },
-    },
+      "200": {
+        description: "Category model instance",
+        content: { "application/json": { schema: getModelSchemaRef(Product) } }
+      }
+    }
   })
   async create(
-    @param.path.number('id') id: typeof Category.prototype.id,
+    @param.path.number("id") id: typeof Category.prototype.id,
     @requestBody({
       content: {
-        'application/json': {
+        "application/json": {
           schema: getModelSchemaRef(Product, {
-            title: 'NewProductInCategory',
-            exclude: ['id'],
-            optional: ['categoryId']
-          }),
-        },
-      },
-    }) product: Omit<Product, 'id'>,
+            exclude: ["id"],
+            optional: ["categoryId"]
+          })
+        }
+      }
+    })
+    product: Omit<Product, "id">
   ): Promise<Product> {
     return this.categoryRepository.products(id).create(product);
   }
 
-  @patch('/categories/{id}/products', {
+  @patch("/categories/{id}/products", {
     responses: {
-      '200': {
-        description: 'Category.Product PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
+      "200": {
+        description: "Category.Product PATCH success count",
+        content: { "application/json": { schema: CountSchema } }
+      }
+    }
   })
   async patch(
-    @param.path.number('id') id: number,
+    @param.path.number("id") id: number,
     @requestBody({
       content: {
-        'application/json': {
-          schema: getModelSchemaRef(Product, {partial: true}),
-        },
-      },
+        "application/json": {
+          schema: getModelSchemaRef(Product, { partial: true })
+        }
+      }
     })
     product: Partial<Product>,
-    @param.query.object('where', getWhereSchemaFor(Product)) where?: Where<Product>,
+    @param.query.object("where", getWhereSchemaFor(Product))
+    where?: Where<Product>
   ): Promise<Count> {
     return this.categoryRepository.products(id).patch(product, where);
   }
 
-  @del('/categories/{id}/products', {
+  @del("/categories/{id}/products", {
     responses: {
-      '200': {
-        description: 'Category.Product DELETE success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
+      "200": {
+        description: "Category.Product DELETE success count",
+        content: { "application/json": { schema: CountSchema } }
+      }
+    }
   })
   async delete(
-    @param.path.number('id') id: number,
-    @param.query.object('where', getWhereSchemaFor(Product)) where?: Where<Product>,
+    @param.path.number("id") id: number,
+    @param.query.object("where", getWhereSchemaFor(Product))
+    where?: Where<Product>
   ): Promise<Count> {
     return this.categoryRepository.products(id).delete(where);
   }
